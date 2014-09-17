aeb82ebbb5623340504a95d9cd4819aa716e7599
unknown <awang4@CATODAWANG1.infor.com>
2014-09-17 16:34:12 -0400

    20140917

---
+++ client/js/app.js
@@ -0,0 +1,15 @@
+// File: /js/app.js
+
+// 'jquery' returns the jQuery object into '$'
+//
+// 'bootstrap' does not return an object. Must appear at the end
+
+require(['jquery', 'bootstrap'], function($){
+
+    // DOM ready
+    $(function(){
+
+        // Twitter Bootstrap 3 carousel plugin
+        $("#element").carousel();
+    });
+});
